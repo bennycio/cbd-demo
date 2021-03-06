afc658ce615e80e6ed4315052953a008b3c4bac6
bennycio <benny@bennyc.io>
2021-03-06 01:08:16 -0800

    Test Serverless for Payment

--- serverless-functions/checkout.js
+++ serverless-functions/checkout.js
@@ -4,8 +4,16 @@ exports.handler = async function(event, context) {
     const body = event.body;
     const auth = process.env.SQUARE_APPLICATION_ID
     console.log(auth);
+    const response = await fetch("https://connect.squareupsandbox.com/v2/payments", {
+        method: 'POST', 
+        headers: {
+            'Authorization': 'Bearer ' + auth,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    });
     return {
         statusCode: 200,
-        body: JSON.stringify({message: 'AUTH' + auth, body: body}),
+        body: JSON.stringify({message: response}),
     };
 }
