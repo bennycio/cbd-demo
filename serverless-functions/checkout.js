bf7b55ac66c0c917e53469be064d488298e8cf36
bennycio <benny@bennyc.io>
2021-03-06 00:51:32 -0800

    Add Debugging For Serverless 3

--- serverless-functions/checkout.js
+++ serverless-functions/checkout.js
@@ -4,8 +4,9 @@ exports.handler = async function(event, context) {
 
     const body = event.body;
     const auth = process.env.REACT_APP_SQUARE_APPLICATION_ID
+    console.log(auth);
     return {
         statusCode: 200,
-        body: JSON.stringify({message: auth}),
+        body: JSON.stringify({message: 'AUTH' + auth, body: body}),
     };
 }
